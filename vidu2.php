c4c1b2af72aedaace9e57d01ce23e600d20bda9a
hoang <hoang@gmail.com>
2024-03-25 14:17:02 +0700

    upload

--- vidu2.php
+++ vidu2.php
@@ -5,7 +5,7 @@ $password = "";
 $DB = "MyDB";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=MyDB", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Connected successfully";
